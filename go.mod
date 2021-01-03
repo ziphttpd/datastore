43144eb4db5d6bdd6f7f4c14c61b188a52b883af
XORveRCOM <contact@xorver.com>
2021-01-04 08:50:58 +0900

    first

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github/ziphttpd.com/ziphttpd/datastore
+
+go 1.11
+
+require (
+	github.com/StringNote/strnote v0.0.0-20201224030209-16cba2762119
+	github.com/xorvercom/util v0.0.0-20200708133029-6df8aabe3148
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/StringNote/strnote/firebase"
+	"github.com/xorvercom/util/pkg/json"
+)
+
+func main() {
+	var (
+		cred = flag.String("cred", "admin.json", "API creditional")
+		col  = flag.String("col", "", "Collection")
+		data = flag.String("dat", "", "Data JSON")
+		imp  = flag.Bool("imp", false, "is Export")
+	)
+	flag.Parse()
+	if *cred == "" || *col == "" {
+		flag.PrintDefaults()
+		return
+	}
+	if *data == "" {
+		data = col
+	}
+	fmt.Printf("cred:\"%s\", col:\"%s\", imp:%v, CommandLine:%v\n", *cred, *col, *imp, flag.CommandLine)
+	firebase.SetOption(*cred)
+	cl := firebase.NewCollection(*col)
+	if *imp == false {
+		elem := json.NewElemObject()
+		for _, key := range cl.Keys() {
+			if val, err := cl.Get(key); err == nil {
+				elem.Put(key, json.NewElemString(val))
+			}
+		}
+		json.SaveToJSONFile(*data+".json", elem, true)
+	} else {
+		if elem, err := json.LoadFromJSONFile(*data + ".json"); err == nil {
+			if obj, ok := elem.AsObject(); ok {
+				for _, key := range obj.Keys() {
+					if str, ok := obj.Child(key).AsString(); ok {
+						cl.Set(key, str.Text())
+					}
+				}
+			}
+		}
+	}
+}
